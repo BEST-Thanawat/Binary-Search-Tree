653d93a41cbab84aa059502069d1a86f5b5c1a34
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-06-01 13:54:00 -0700

    Add project files.

---
+++ Binary Search Tree/BinarySearchTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node _root;
+        public BinarySearchTree()
+        {
+            _root = null;
+        }
+        public void Insert(int i)
+        {
+            //Create new node
+            Node newNode = new Node();
+            newNode.Data = i;
+
+            //Return newNode as a root
+            if (_root == null) _root = newNode;
+
+            //Loop through the tree
+            else
+            {
+                Node current = _root;
+                Node parent = null;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data) //Left Node
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.Right; //Right Node
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Traversal the tree
+        public void Traverse(Node node)
+        {
+            if (node == null) return;
+
+            Console.WriteLine(node.Data);
+            Traverse(node.Left);
+            Traverse(node.Right);
+        }
+    }
+}
---
+++ Binary Search Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(10);
+            bst.Insert(11);
+            bst.Insert(9);
+            bst.Insert(12);
+            bst.Insert(19);
+            bst.Insert(14);
+            bst.Insert(18);
+            bst.Insert(21);
+            bst.Insert(2);
+            bst.Insert(27);
+            bst.Insert(22);
+            bst.Insert(13);
+
+            bst.Traverse(bst._root);
+            Console.ReadLine();
+        }
+    }
+}
